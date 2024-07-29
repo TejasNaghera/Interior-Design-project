1f8dfe4971a1381602fff416b819c45fa2dfd958
TejasNaghera <152476032+TejasNaghera@users.noreply.github.com>
2024-07-29 23:48:17 +0530

    Add files via upload

---
+++ jq.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    ////////////////empty/////////////////////////
+    $(".nav-btn").click(function(){
+        //  $('.R-btn>i').css("display", "none")
+        $(".menu").toggleClass("reyal")
+        
+        // $(".fa-solid ").css('display',' none')
+    })
+    // $('.ul-box>li').click(function(){
+    //    $('.ul-box').removeClass("reyal") 
+    // })
+})
